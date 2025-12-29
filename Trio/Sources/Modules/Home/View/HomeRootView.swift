7a5805c30a45b63a8fe11696387d7c8f1a2d5080
ejm0 <eliasjosefmaurer@gmail.com>
2025-12-29 17:00:28 +0100

    fix no focusing of fields

--- Trio/Sources/Modules/Home/View/HomeRootView.swift
+++ Trio/Sources/Modules/Home/View/HomeRootView.swift
@@ -1074,7 +1074,6 @@ extension Home {
             }
             .navigationTitle("Home")
             .navigationBarHidden(true)
-            .ignoresSafeArea(.keyboard)
             .blur(radius: state.isLoopStatusPresented ? 3 : 0)
             .sheet(isPresented: $state.isLoopStatusPresented) {
                 LoopStatusView(state: state)
